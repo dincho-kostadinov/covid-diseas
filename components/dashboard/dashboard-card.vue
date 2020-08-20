adc2ab5a9b79f993367ebb5364fbf0cd2243acb1
prime.dincho-at-405746926396 <prime.dincho@partymus.com>
2020-08-20 14:16:16 +0300

    refactor details page and clean some code

--- components/dashboard/dashboard-card.vue
+++ components/dashboard/dashboard-card.vue
@@ -19,23 +19,4 @@ export default {
 </script>
 
 <style>
-/* .container {
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-} */
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
--- pages/countryDetails/_id.vue
+++ pages/countryDetails/_id.vue
@@ -1,24 +1,37 @@
 <template>
-  <div>
-    <h1 class="title">Tracking Coronavirus (COVID-19) for</h1>
+  <v-container class="grey lighten-5">
+    <v-row>
+      <v-col>
+        <h1 class="title">
+          Tracking Coronavirus (COVID-19) for {{ this.$route.params.id }}
+        </h1>
+      </v-col>
+    </v-row>
 
-    <v-btn small v-on:click="getTodayData">Today</v-btn>
-    <v-btn small v-on:click="getYesterdayData">Yesterday</v-btn>
-    <v-btn small v-on:click="getTwoDaysAgoData">Before 2 days</v-btn>
-    <v-data-table
-      :headers="headers"
-      :items="[countrieDeatils]"
-      :items-per-page="1"
-      @click:row="handleClick"
-      class="elevation-1"
-    ></v-data-table>
-  </div>
+    <v-row>
+      <v-col>
+        <v-btn small v-on:click="getData">Today</v-btn>
+        <v-btn small v-on:click="getData('yesterday')">Yesterday</v-btn>
+        <v-btn small v-on:click="getData('twoDaysAgo')">Before 2 days</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-data-table
+          :headers="headers"
+          :items="[countrieDeatils]"
+          :items-per-page="1"
+          @click:row="handleClick"
+          class="elevation-1"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 const axios = require("axios");
-const $router = require("vue-router");
-import dashboardCard from "../../components/dashboard/dashboard-card";
+
 export default {
   data() {
     return {
@@ -34,55 +47,35 @@ export default {
         { text: "Total Deaths", value: "deaths" },
         { text: "New Deaths", value: "todayDeaths" },
         { text: "Total Recovered", value: "recovered" },
-        { text: "Active Cases", value: "active" }
+        { text: "Active Cases", value: "active" },
+        { text: "One test per people", value: "oneTestPerPeople" },
+        { text: "Recovered per One Million", value: "recoveredPerOneMillion" },
+        { text: "Tests", value: "tests" }
       ],
       countrieDeatils: {}
     };
   },
-  components: {
-    dashboardCard: dashboardCard
-  },
+  components: {},
   methods: {
-    getYesterdayData: function() {
+    getData: function(filter) {
+      var url = "";
+      if (filter) {
+        this.url = `https://disease.sh/v3/covid-19/countries/${this.$route.params.id}?${filter}=true&strict=true`;
+      } else {
+        this.url = `https://disease.sh/v3/covid-19/countries/${this.$route.params.id}?strict=true`;
+      }
       axios
-        .get(
-          `https://disease.sh/v3/covid-19/countries/Afghanistan?strict=true?yesterday=true`
-        )
+        .get(this.url)
         .then(response => {
           this.countrieDeatils = response.data;
         })
         .catch(e => {
           this.errors.push(e);
         });
-    },
-    getTwoDaysAgoData: function() {
-      axios
-        .get(
-          `https://disease.sh/v3/covid-19/countries/Afghanistan?strict=true?twoDaysAgo=true`
-        )
-        .then(response => {
-          this.countrieDeatils = response.data;
-        })
-        .catch(e => {
-          this.errors.push(e);
-        });
-    },
-    getTodayData: function() {
-      axios
-        .get(`https://disease.sh/v3/covid-19/countries/Afghanistan?strict=true`)
-        .then(response => {
-          this.countrieDeatils = response.data;
-        })
-        .catch(e => {
-          this.errors.push(e);
-        });
-    },
-    handleClick: function(value) {
-      console.log(value);
     }
   },
   created() {
-    this.getTodayData();
+    this.getData();
   }
 };
 </script>
